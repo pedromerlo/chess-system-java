064facba90d8ddf4d0743589b3ef9602bd08f136
pedromerlo <pedro.merlo.junior@gmail.com>
2024-06-22 12:50:51 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,9 +1,8 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
-import chess.pieces.King;
-import chess.pieces.Rook;
+import pieces.King;
+import pieces.Rook;
 
 public class ChessMatch {
     private Board board;
@@ -23,10 +22,14 @@ public class ChessMatch {
         return mat;
     }
 
+
+    private void placeNewPiece (char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1 ));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4 ));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,4 ));
-        
+        placeNewPiece('b',6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8,new King(board, Color.BLACK));
+        placeNewPiece('e', 1 ,new King(board, Color.BLACK));
     }
 }
--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -1,17 +1,17 @@
-package chess.pieces;
+package pieces;
 
 import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class Rook extends ChessPiece {
 
-    public King(Board board, Color color) {
+    public Rook(Board board, Color color) {
         super(board, color);
     }
 
     @Override
     public String toString() {
-        return "K";
+        return "R";
     }
 }
