9f94fca1023bab0b4c13841e16bce56afef3bdc5
pedromerlo <pedro.merlo.junior@gmail.com>
2024-06-25 11:59:37 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()){
+			System.out.println("CHECK!");
+		}
 
 	}	
 
@@ -106,8 +109,8 @@ public class UI {
 	}
 
 	private static void printCapturedPieces(List<ChessPiece> captured){
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		List<ChessPiece> white = captured.stream().filter(x -> (x != null && x.getColor() == Color.WHITE )).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> (x != null && x.getColor() == Color.BLACK )).collect(Collectors.toList());
 		System.out.println("Captured pieces: ");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
